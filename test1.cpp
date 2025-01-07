2babbdc28688791683a824275ea8fae15d4920d7
5-12 <hellowordare@163.com>
2025-01-07 17:09:38 +0800

    测试1

--- test1.cpp
+++ test1.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 using namespace std;
 int test(int);
+int a0[100];
 int main(void)
 {
-    test(0);
+    test(1);
 }
-
+int test(int a)
+{
+    for (int i = 1; i <= 3; i++)
+    {
+        if (a<=3)
+        {
+            a0[a]=i;
+            test(a+1);
+        }
+        else
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                cout<<a0[i]<<" ";
+                
+            }
+            cout<<endl;
+            return 0;
+        }
+    }
+    return 0;
+}

fce9a5e8e8fd83625b2c29289aa67e2f0ca4a8b3
5-12 <hellowordare@163.com>
2025-01-10 20:38:54 +0800

    二进制转换

---
+++ 2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int a[20]={1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+bool a1[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int n=0;
+int main()
+{
+    int b=1;
+    for (int i = 0; i <=20; i++)
+    {
+        //a[i]*=a[i-1]*2;
+        //cout<<a[i-1]*2<<" "<<i<<" "<<a[i-1]<<endl;
+        a[i]=b;
+        b*=2;
+        //cout<<b<<endl;
+    }
+    cin>>n;
+    for (int i = 20; i>=0; i--)
+    {
+        //cout<<a[i]<<endl;
+        if(n>=a[i])
+        {
+            ;
+            a1[i]=1;
+            n-=a[i];
+            //cout<<"n>"<<a[i]<<"n="<<n<<endl;
+            
+        }
+        
+        //;cout<<a[i]<<endl;
+    }
+    for (int i = 20; i >=0; i--)
+    {
+        cout<<a1[i];
+    }
+    
+    return 0;
+    
+}
